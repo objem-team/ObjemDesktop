70e26fb1c773de04a46ac3af729605ef91d00fc5
kinjikcs <kcska_1817@kcska.onmicrosoft.com>
2021-11-19 13:36:50 +0900

    add:QRコード生成する関数

---
+++ ObjemDesktop/QrGenerater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing;
+using System.Net;
+
+namespace ObjemDesktop
+{
+    class QrGenerater
+    {   
+        
+        //QRコード取得
+        public Image Generate(String Str,int Width, int Height)
+        {
+            var Writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    QrVersion = 5,
+                    ErrorCorrection = ErrorCorrectionLevel.M,
+                    CharacterSet = "UTF-8",
+                    Width = Width,
+                    Height = Height,
+                    Margin = 5,
+                },
+            };
+            return Writer.Write(Str);
+        }
+    }
+}
