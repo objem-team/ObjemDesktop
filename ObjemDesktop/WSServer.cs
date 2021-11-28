e379763874e28cdca35a8eff63ca7bb3d31b3c15
shiguma127 <shiguma127@gmail.com>
2021-11-28 23:18:24 +0900

    add:WSServer

---
+++ ObjemDesktop/WSServer.cs
@@ -0,0 +1,30 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjemDesktop
+{
+    class WSServer
+    {
+        public delegate void OnOpenDelegate(object sender);
+        public delegate void onCloseDelegate(object sender);
+        public delegate void onMessageDelegate(object sender, String message);
+
+        public event OnOpenDelegate OnOpen;
+        public event onCloseDelegate OnClose;
+        public event onMessageDelegate OnMessage;
+        public void start()
+        {
+            var server = new WebSocketServer("ws://127.0.0.1:8000");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => OnOpen?.Invoke(this);
+                socket.OnClose = () => OnClose?.Invoke(this);
+                socket.OnMessage = message => OnMessage?.Invoke(this,message);
+            });
+        }
+    }
+}
