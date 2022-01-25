1177718c4cd65fc3fd7fa1b01a402a2911325c37
shiguma127 <shiguma127@gmail.com>
2022-01-26 01:37:01 +0900

    add:ListUtil 含まれていれば上書き、含まれていなければ追加するメソッドを追加

---
+++ ObjemDesktop/ListUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjemDesktop
+{
+    public class ListUtil
+    {
+        public static IEnumerable<T> AddOrReplace<T>(List<T> array, T value) where T : IEquatable<T>
+        {
+            var index = array.FindIndex(s => s.Equals(value));
+            if (index == -1)
+            {
+                array.Add(value);
+            }
+            else
+            {
+                array[index] = value;
+            }
+            
+            return array;
+        }
+    }
+}
