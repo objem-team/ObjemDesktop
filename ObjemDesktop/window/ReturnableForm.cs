4aefcb45ebee94d9f25bcb04a1c4a4114275c1bb
shiguma127 <shiguma127@gmail.com>
2022-01-25 12:12:07 +0900

    add:RetunableForm

---
+++ ObjemDesktop/window/ReturnableForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ObjemDesktop.window
+{
+    public class ReturnableValueForm<T> : Form
+    {
+        internal readonly Action<T> Callback;
+        protected ReturnableValueForm(Action<T> callback)
+        {
+            Callback = callback;
+        }
+    }
+}
